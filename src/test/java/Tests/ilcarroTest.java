a5115fad1617470bf2bdc98e345495eac63bde02
rageULV <esawein5@gmail.com>
2024-03-08 12:24:40 +0200

    hm 08.03.24

--- src/test/java/Tests/ilcarroTest.java
+++ src/test/java/Tests/ilcarroTest.java
@@ -1,14 +1,18 @@
 package Tests;
 
 import Config.BaseTest;
+import Helpers.TopMenuItem;
 import Pages.MainPage;
+import Pages.SearchPage;
 import org.testng.annotations.Test;
 import org.testng.reporters.jq.Main;
 
 public class ilcarroTest extends BaseTest {
 
-    @Test void ilcarroTest_001(){
+    @Test void ilcarroTest_001() throws InterruptedException{
         MainPage mainPage = new MainPage(getDriver());
+        SearchPage searchPage = mainPage.openTopMenu(TopMenuItem.SEARCH.toString());
+        Thread.sleep(3000);
 
     }
 }
